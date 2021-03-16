486f9182b0441df22aff9628afc1817204148573
manojsatya <manoj.pesit@gmail.com>
2021-03-16 20:05:26 +0100

    Configure tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      body: ['Nunito'],
+      body: ["Nunito"],
     },
   },
   variants: {
