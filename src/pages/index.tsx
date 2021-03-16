5222134a6a65c78331bca328fa6edffe80923f3b
manojsatya <manoj.pesit@gmail.com>
2021-03-16 20:06:26 +0100

    Add Next.js files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,38 @@
-import React from 'react';
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import imgSrc from '../images/portfolioImage.jpeg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
+import React from "react";
+import Layout from "../components/layout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library, IconName } from "@fortawesome/fontawesome-svg-core";
 import {
   faLinkedin,
   faGithub,
   faXing,
   faCodepen,
-} from '@fortawesome/free-brands-svg-icons';
+} from "@fortawesome/free-brands-svg-icons";
 
 library.add(faGithub, faLinkedin, faXing, faCodepen);
 
 const icons = [
   {
-    name: 'github',
-    href: 'https://github.com/manojsatya',
+    name: "github",
+    href: "https://github.com/manojsatya",
   },
   {
-    name: 'linkedin',
-    href: 'https://www.linkedin.com/in/manojsatya',
+    name: "linkedin",
+    href: "https://www.linkedin.com/in/manojsatya",
   },
   {
-    name: 'xing',
-    href: 'https://www.xing.com/profile/Manoj_Satyanarayana/cv',
+    name: "xing",
+    href: "https://www.xing.com/profile/Manoj_Satyanarayana/cv",
   },
-  { name: 'codepen', href: 'https://codepen.io/manu_js' },
+  { name: "codepen", href: "https://codepen.io/manu_js" },
 ];
 
 const IndexPage: React.FC = () => (
   <Layout>
-    <SEO title="Home" />
     <div className="flex justify-center items-center m-auto h-screen">
       <div className="grid text-center">
         <img
-          src={imgSrc}
+          src="/portfolioImage.jpeg"
           alt="portfolio"
           className="max-w-xs rounded-full mx-auto"
         />
@@ -44,20 +41,20 @@ const IndexPage: React.FC = () => (
           Hamburg, Germany
         </h3>
         <div className="flex justify-evenly m-5">
-          {icons.map(icon => {
+          {icons.map((icon) => {
             const { name, href } = icon;
             return (
-              <>
+              <div key={name}>
                 <a
                   href={href}
                   target="__blank"
-                  style={{ textDecoration: 'none', color: 'inherit' }}
+                  style={{ textDecoration: "none", color: "inherit" }}
                   aria-label={name}
                   title={name}
                 >
-                  <FontAwesomeIcon icon={['fab', name]} size="3x" />
+                  <FontAwesomeIcon icon={["fab", name as IconName]} size="3x" />
                 </a>
-              </>
+              </div>
             );
           })}
         </div>
