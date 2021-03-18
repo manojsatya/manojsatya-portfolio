e8088402bbb110e760ce2052bf096fa0ec5683bd
manojsatya <manoj.pesit@gmail.com>
2021-03-18 22:42:26 +0100

    Remove src folder

--- components/header.js
+++ components/header.js
@@ -1,18 +1,9 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import React from "react";
 
 const Header = () => (
   <header className="bg-indigo-400 w-screen">
     <h1 className="max-w-screen-lg m-auto">Manoj Satyanarayana</h1>
   </header>
 );
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
-
-Header.defaultProps = {
-  siteTitle: ``,
-};
 
 export default Header;
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const Layout: React.FC = ({ children }) => {
   return (
@@ -7,16 +7,16 @@ const Layout: React.FC = ({ children }) => {
       <main className="max-w-screen-lg m-auto">{children}</main>
       <footer
         style={{
-          position: 'absolute',
-          bottom: '0px',
-          margin: 'auto',
-          textAlign: 'center',
-          width: '100%',
+          position: "absolute",
+          bottom: "0px",
+          margin: "auto",
+          textAlign: "center",
+          width: "100%",
         }}
       >
         © {new Date().getFullYear()}, Built with &#10084;&#65039;
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <a href="https://nextjs.org/">Next.js</a>
       </footer>
     </div>
   );
