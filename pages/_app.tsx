d1429c5165d9df6f531ef7aace37377fce611ad4
manojsatya <manoj.pesit@gmail.com>
2021-03-26 18:27:32 +0100

    configure prismjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import "prismjs/themes/prism-tomorrow.css";
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
