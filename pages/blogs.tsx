81aa42273a8f16137d5b786c0762176410a7f294
manojsatya <manoj.pesit@gmail.com>
2021-03-26 18:27:20 +0100

    Add TS support to blogpage

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import Link from "next/link";
-import { getAllPosts } from "../lib/posts";
+import { getAllPosts, IallPosts } from "../lib/posts";
+import { GetStaticProps } from "next";
 
-export default function Blogs({ allPosts }) {
+interface BlogsProps {
+  allPosts: IallPosts[];
+}
+
+export default function Blogs({ allPosts }: BlogsProps) {
   return (
     <div>
       <p>Blogs page test</p>
       {allPosts.map((e) => (
         <div key={e.slug}>
           <Link href={`/blog/${e.slug}`}>{e.title}</Link>
-          <p>{e.content}</p>
         </div>
       ))}
     </div>
   );
 }
 
-export async function getStaticProps(context) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const allPosts = getAllPosts();
   return {
     props: { allPosts }, // will be passed to the page component as props
   };
-}
+};
